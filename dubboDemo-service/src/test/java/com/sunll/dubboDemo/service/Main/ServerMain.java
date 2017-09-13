b99865cef7cd8891930510e5b7c5a851bbb74ec4
sunll <sunl@jvtd.cn>
2017-09-13 18:13:57 +0800

    添加service打包

---
+++ dubboDemo-service/src/test/java/com/sunll/dubboDemo/service/Main/ServerMain.java
@@ -0,0 +1,15 @@
+package com.sunll.dubboDemo.service.Main;
+
+import com.alibaba.dubbo.container.Main;
+
+import java.io.IOException;
+
+/**
+ * Created by Administrator
+ * on 2017/9/8
+ */
+public class ServerMain {
+    public static void main(String[] args) throws IOException {
+        Main.main(args);
+    }
+}
